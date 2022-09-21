f81290060fbf59a8bb9fd0e826835051709f7694
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2022-09-21 10:58:10 +0300

    add LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,36 @@ import { Contacts } from './Contacts/Contacts';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '777-77-70' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
   };
+
+  componentDidMount() {
+    if (localStorage.getItem('contacts')) {
+      this.setState(() => {
+        return {
+          contacts: [...JSON.parse(localStorage.getItem('contacts'))],
+        };
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   onGetDataForm = data => {
     const hasName = this.state.contacts.some(
       el => el.name.toLowerCase() === data.name.toLowerCase()
     );
     if (hasName) {
-      Notify.warning(`Contact "${data.name}" is already exist.`);
+      Notify.warning(`Contact "${data.name}" is already in contacts.`);
       return;
     }
 
@@ -33,14 +54,14 @@ export class App extends Component {
   render() {
     const { contacts } = this.state;
     return (
-      <div>
+      <>
         <Section title="Phonebook">
           <ContactForm onSubmit={this.onGetDataForm} />
         </Section>
         <Section title="Contacts">
           <Contacts contacts={contacts} onClickDelete={this.deleteItem} />
         </Section>
-      </div>
+      </>
     );
   }
 }
